d271e28b80d30408b1a273c54a66fac1851a7781
Molato Sekgobela <molato.sekgobela@gmail.com>
2024-02-03 09:35:16 +0200

    add work flows

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u%yy)qornxl%a=px@7u#ab@d##v$fi7$-z&)fu28=80j#%ur7+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["s-creations.co.za"]
 
